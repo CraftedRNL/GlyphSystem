8a37cf3214410011c35432b297da416f5acda746
CraftedRNL <126Rliu@frhsd.com>
2026-01-23 08:23:13 -0500

    added

--- js/java.js
+++ js/java.js
@@ -8,7 +8,7 @@ console.log("2")
         const glyphs = data.glyphs;
         const container = document.getElementById("cards");
         
-        glyphs.forEach(glyph => {
+        glyphs.blessing.forEach(glyph => {
             const card = document.createElement("div");
             //add a className "card" to the card variable
             card.className = "card";
@@ -41,6 +41,41 @@ console.log("2")
             container.appendChild(card); //variables go into those spots
             
         });
+
+        glyphs.curses.forEach(glyph => {
+            const card = document.createElement("div");
+            //add a className "card" to the card variable
+            card.className = "card";
+            
+            let godsHTML = "";
+
+            for (let i = 0; i < glyph.gods.length; i++) {
+                
+                
+                    godsHTML +=  "[" + glyph.gods[i] +"] " ;
+                
+            }
+            
+            card.innerHTML = `
+                <h2>
+                ${glyph.name}
+                </h2>
+                <img src="${glyph.img}" alt="card">
+                <p><span class="label">[${glyph.tier}] ${godsHTML} [${glyph.myth}]
+                [${glyph.type}] [${glyph.effect}]
+                </span>
+                </p>
+                <p><span class="label"><i>${glyph.quote}</i></span>
+                
+                </p>
+                <p><span class="label"><i>${glyph.ability}</i></span>
+                
+                </p>`;
+
+            container.appendChild(card); //variables go into those spots
+            
+        });
+
     }
 };
 
